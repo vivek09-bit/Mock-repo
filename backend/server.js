42f23d34c67e97b25da267c84d033c3859ee13da
Vivek <64359247+vivek09-bit@users.noreply.github.com>
2025-03-25 18:55:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
